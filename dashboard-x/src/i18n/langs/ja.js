4abeff544fe0688e21bf3cb069296774c008cf7e
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2025-10-28 09:19:33 +0300

    Create ja.js

---
+++ dashboard-x/src/i18n/langs/ja.js
@@ -0,0 +1,10 @@
+export default {
+  systemTitle:"システム管理", login:"ログイン", emailOrPhone:"メールまたは電話",
+  password:"パスワード", signIn:"サインイン", logout:"ログアウト", language:"言語",
+  helpOn:"ヘルプ ON", helpOff:"ヘルプ OFF", stats:"ライブ統計", users:"ユーザー",
+  joeActivity:"Joe のアクティビティ", activeNow:"現在オンライン", totals:"合計", supers:"スーパー管理者",
+  admins:"管理者", normalUsers:"ユーザー", online:"オンライン", redisOnline:"Redis", mongoOnline:"MongoDB",
+  sendCommand:"コマンド送信", commandPlaceholder:"Joe へのコマンドを入力…", send:"送信",
+  welcome:"お帰りなさい", session:"セッション", copyToken:"トークンをコピー", tokenCopied:"コピーしました",
+  approve:"承認", deny:"却下"
+}
