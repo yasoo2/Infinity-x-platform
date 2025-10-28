ad0744e6ea1739a112b6b474a9b32103e4009ffc
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2025-10-28 09:16:47 +0300

    Create zh.js

---
+++ dashboard-x/src/i18n/langs/zh.js
@@ -0,0 +1,9 @@
+export default {
+  systemTitle:"系统管理", login:"登录", emailOrPhone:"邮箱或电话",
+  password:"密码", signIn:"登录", logout:"退出", language:"语言", helpOn:"帮助开启",
+  helpOff:"帮助关闭", stats:"实时统计", users:"用户", joeActivity:"Joe 活动",
+  activeNow:"当前在线", totals:"总计", supers:"超级管理员", admins:"管理员", normalUsers:"用户",
+  online:"在线", redisOnline:"Redis", mongoOnline:"MongoDB", sendCommand:"发送指令",
+  commandPlaceholder:"给 Joe 输入指令…", send:"发送", welcome:"欢迎回来", session:"会话",
+  copyToken:"复制令牌", tokenCopied:"已复制", approve:"批准", deny:"拒绝"
+}
