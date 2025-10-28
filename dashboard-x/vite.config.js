20e29bdd0553cb0726c8d2a474437f5d771578ca
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2025-10-28 13:28:30 +0300

    Update vite.config.js

--- dashboard-x/vite.config.js
+++ dashboard-x/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// هون بنحدد الـ API تبع الباكند تبعنا
+const API_BASE = 'https://api.xelitesolutions.com'
+
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    port: 5173
+  define: {
+    __API_BASE__: JSON.stringify(API_BASE),
   }
 })
