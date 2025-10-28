9ba7a35e1754bef910a604a36954c585a51713ab
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2025-10-28 09:15:17 +0300

    Create de.js

---
+++ dashboard-x/src/i18n/langs/de.js
@@ -0,0 +1,10 @@
+export default {
+  systemTitle:"Systemverwaltung", login:"Anmelden", emailOrPhone:"E-Mail oder Telefon",
+  password:"Passwort", signIn:"Einloggen", logout:"Abmelden", language:"Sprache",
+  helpOn:"Hilfe AN", helpOff:"Hilfe AUS", stats:"Live-Statistiken", users:"Benutzer",
+  joeActivity:"Joe-Aktivität", activeNow:"Jetzt aktiv", totals:"Gesamt", supers:"Super-Admins",
+  admins:"Admins", normalUsers:"Benutzer", online:"Online", redisOnline:"Redis", mongoOnline:"MongoDB",
+  sendCommand:"Befehl senden", commandPlaceholder:"Befehl an Joe schreiben…", send:"Senden",
+  welcome:"Willkommen zurück", session:"Sitzung", copyToken:"Token kopieren", tokenCopied:"Kopiert",
+  approve:"Genehmigen", deny:"Ablehnen"
+}
