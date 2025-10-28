ba4765930a1f452fafc9f92a1217c4638c7aede9
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2025-10-28 23:53:26 +0300

    Update main.jsx

--- dashboard-x/src/main.jsx
+++ dashboard-x/src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react"
-import { createRoot } from "react-dom/client"
-import App from "./App.jsx"
-import "./styles.css"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+const rootEl = document.getElementById("root");
+createRoot(rootEl).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
