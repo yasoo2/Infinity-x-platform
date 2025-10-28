3d23e1e1d2cd315bd3f6e27489608a8445bbe2e2
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2025-10-28 09:17:42 +0300

    Create hi.js

---
+++ dashboard-x/src/i18n/langs/hi.js
@@ -0,0 +1,9 @@
+export default {
+  systemTitle:"सिस्टम एडमिन", login:"लॉगिन", emailOrPhone:"ईमेल या फोन",
+  password:"पासवर्ड", signIn:"साइन इन", logout:"लॉगआउट", language:"भाषा", helpOn:"हेल्प चालू",
+  helpOff:"हेल्प बंद", stats:"लाइव आँकड़े", users:"यूज़र्स", joeActivity:"जो की गतिविधि",
+  activeNow:"अभी सक्रिय", totals:"कुल", supers:"सुपर एडमिन", admins:"एडमिन", normalUsers:"यूज़र्स",
+  online:"ऑनलाइन", redisOnline:"Redis", mongoOnline:"MongoDB", sendCommand:"कमांड भेजें",
+  commandPlaceholder:"जो को कमांड लिखें…", send:"भेजें", welcome:"वापसी पर स्वागत", session:"सेशन",
+  copyToken:"टोकन कॉपी", tokenCopied:"कॉपी हुआ", approve:"स्वीकृत", deny:"अस्वीकृत"
+}
