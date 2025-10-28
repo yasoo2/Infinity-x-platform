014ac140cdb4502068d5f7b1fa6fa068701fef9a
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2025-10-28 09:20:21 +0300

    Create id.js

---
+++ dashboard-x/src/i18n/langs/id.js
@@ -0,0 +1,9 @@
+export default {
+  systemTitle:"Admin Sistem", login:"Masuk", emailOrPhone:"Email atau Telepon",
+  password:"Kata sandi", signIn:"Login", logout:"Keluar", language:"Bahasa", helpOn:"Bantuan Aktif",
+  helpOff:"Bantuan Nonaktif", stats:"Statistik Langsung", users:"Pengguna", joeActivity:"Aktivitas Joe",
+  activeNow:"Aktif sekarang", totals:"Total", supers:"Super Admin", admins:"Admin", normalUsers:"Pengguna",
+  online:"Online", redisOnline:"Redis", mongoOnline:"MongoDB", sendCommand:"Kirim Perintah",
+  commandPlaceholder:"Ketik perintah untuk Joe…", send:"Kirim", welcome:"Selamat datang kembali",
+  session:"Sesi", copyToken:"Salin token", tokenCopied:"Tersalin", approve:"Setujui", deny:"Tolak"
+}
