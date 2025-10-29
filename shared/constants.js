d429b652ba21d7c4470847002c912ddd2db40226
yasoo2 <86189296+yasoo2@users.noreply.github.com>
2025-10-30 00:53:40 +0300

    ddfddrd

--- shared/constants.js
+++
@@ -1,5 +0,0 @@
-export const COMPANY_NAME_DEFAULT = "X Elite Solutions / Future Systems";
-export const SYSTEM_TITLE_EN = "System Admin";
-export const SYSTEM_TITLE_AR = "إدارة النظام";
-export const DEFAULT_LANG = "en";
-export const HELP_MODE_DEFAULT = true;
