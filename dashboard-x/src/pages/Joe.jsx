5c7b7e22c196a4938c774683cddd016f3319f372
Younes Sowady <younes.sowady@example.com>
2025-11-05 00:45:35 -0500

    fix: Restore Joe.jsx to working version before JoeComputer

--- dashboard-x/src/pages/Joe.jsx
+++ dashboard-x/src/pages/Joe.jsx
@@ -1,12 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react';
-import JoeComputer from '../components/JoeComputer.jsx';
-
 import ChatSidebar from '../components/ChatSidebar';
 import FileUpload from '../components/FileUpload';
 import { useJoeChat } from '../hooks/useJoeChat.js';
 import JoeScreen from '../components/JoeScreen.jsx';
-
-
 
 const Joe = () => {
   const messagesEndRef = useRef(null);
@@ -17,10 +13,6 @@ const Joe = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isScreenVisible, setIsScreenVisible] = useState(false);
   const [isTakeoverActive, setIsTakeoverActive] = useState(false);
-  const [isJoeComputerVisible, setIsJoeComputerVisible] = useState(true);
-
-
-
   
   const {
     userId,
@@ -65,31 +57,8 @@ const Joe = () => {
     scrollToBottom();
   }, [messages]);
 
-  const handleCloseJoeComputer = () => {
-    setIsJoeComputerVisible(false);
-  };
-
-  const handleComputerTakeover = () => {
-    console.log('User has taken over the Joe computer.');
-  };
-
-
-
-
-
-
-
   return (
     <>
-      {isJoeComputerVisible && (
-        <JoeComputer
-          isVisible={isJoeComputerVisible}
-          onClose={handleCloseJoeComputer}
-          onTakeover={handleComputerTakeover}
-        />
-      )}
-
-
     <div className="min-h-[calc(100vh-4rem)] flex flex-col md:flex-row bg-gray-950 text-white relative">
       {/* Mobile Sidebar Toggle Button */}
       <button
